e8f6671dfa0779883d4ac7ef503ab2624b073831
Onur Köse <103755656+Onur-Kose@users.noreply.github.com>
2023-03-29 10:31:02 +0300

    giriş ekranı düzeltmesi

--- Icaz.com/Models/View_Halper/LoginUser.cs
+++ Icaz.com/Models/View_Halper/LoginUser.cs
@@ -1,8 +1,10 @@
-﻿namespace Icaz.com.Models.View_Halper
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Icaz.com.Models.View_Halper
 {
     public class LoginUser
     {
-
+        [EmailAddress(ErrorMessage ="e posta adresi epposat şeklinde olmalıdır.")]
         public string Eposta { get; set; }
         public string Password { get; set; }
     }
